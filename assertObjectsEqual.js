28c1e82458f8715f21dcb1a35129bf8b60cb6f46
William Li <110069721+william-li0128@users.noreply.github.com>
2022-09-29 10:19:41 -0700

    object assertion tool created.

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -28,9 +28,19 @@ const eqObjects = function(object1, object2) {
       if (object1[key] !== object2[key]) {
         return false;
       }
-   }
+    }
   }
   return true;
+};
+
+// function implementation
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🛑🛑🛑  Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+  }
 };
 
 //`🛑🛑🛑  Assertion Failed: ${object1} !== ${object2}`
@@ -40,14 +50,14 @@ const eqObjects = function(object1, object2) {
 //Test code below
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
-console.log(eqObjects(ab, ba)); // => true
+assertObjectsEqual(ab, ba); // => true
 
 const abc = { a: "1", b: "2", c: "3" };
-console.log(eqObjects(ab, abc)); // => false
+assertObjectsEqual(ab, abc); // => false
 
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
-console.log(eqObjects(cd, dc)); // => true
+assertObjectsEqual(cd, dc); // => true
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
-console.log(eqObjects(cd, cd2)); // => false
+assertObjectsEqual(cd, cd2); // => false
