7eee2eeb2cf8bc5a4eb5d501161f47edc6ff5d33
William Li <110069721+william-li0128@users.noreply.github.com>
2022-10-12 13:30:23 -0700

    headTest.js modified with M & C package.

--- test/headTest.js
+++ test/headTest.js
@@ -1,9 +1,36 @@
-// test/assertEqualTest.js
-const assertEqual = require('../assertEqual');
-const head = require('../head');
+// const assertEqual = require('../assertEqual');
+// const head = require('../head');
 
-//test assertions
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([2]), 2);
-assertEqual(head(), undefined);
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([2]), 2);
+// assertEqual(head(), undefined);
+
+const assert = require('chai').assert;
+const head   = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+  it("return 5 for [5, 6, 7]", () => {
+    assert.strictEqual(head([5,6,7]), 5);
+  });
+
+  it("return 2 for [2]", () => {
+    assert.strictEqual(head([2]), 2);
+  });
+
+  it("return undefined for null", () => {
+    assert.strictEqual(head(), undefined);
+  });
+
+  it("return 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+});
